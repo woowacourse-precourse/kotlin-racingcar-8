9b6350e24fdb9cee8ef185240c19f7b24dbfed62
Chaeun Kim <kimchaeun111@gmail.com>
2025-10-21 17:40:39 +0900

    feat: 자동차 이름 입력 기능 구현
    사용자로부터 쉼표로 구분된 자동차 이름을 입력받아
    `List<Car>` 객체를 생성하는 기능을 구현합니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+class Car(val name: String) {
+
+}
+
 fun main() {
     // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNames = Console.readLine().split(",")
+    val cars = carNames.map { Car(it.trim()) }
 }
