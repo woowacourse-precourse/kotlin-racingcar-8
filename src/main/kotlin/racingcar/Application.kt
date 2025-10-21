1f3b5d6243e5edc25478eba0309f6d4876cc0d31
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-21 19:53:26 +0900

    feat: 자동차 경주를 실행시키는 main함수 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console as Console
 
 fun main() {
-    // TODO: 프로그램 구현
+    val inputController = InputController()
+
+    View.inputName() // 이름 입력
+    val carList = inputController.createCarList(Console.readLine()) // 자동차 리스트
+
+    View.inputCount() // 움직임 값 입력
+    val count = inputController.countToInt(Console.readLine()) // 움직임 값
+
+    val carController = CarController(carList) // 자동차 컨트롤러
+    View.movingResult(carController, count) // 실행 결과
+    View.result(carController) // 최종 우승자
+
 }
