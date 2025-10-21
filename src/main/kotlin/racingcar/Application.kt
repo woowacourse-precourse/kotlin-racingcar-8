c158251feaf0a78fd3b4f0a36cd4a0f055fed287
Chaeun Kim <kimchaeun111@gmail.com>
2025-10-21 17:46:47 +0900

    feat: 시도할 횟수 입력 기능 구현
    사용자로부터 시도할 횟수를 입력받는 기능을 추가합니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -11,4 +11,7 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carNames = Console.readLine().split(",")
     val cars = carNames.map { Car(it.trim()) }
+
+    println("시도할 횟수는 몇 회인가요?")
+    val tryCount = Console.readLine().toInt()
 }
