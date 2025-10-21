ee7d837cabd84fc7c0ae3acd4fab991945fcedcf
hyeonjune <annhj980@naver.com>
2025-10-22 04:00:53 +0900

    feat: 횟수 입력 기능 구현

--- src/main/kotlin/racingcar/util/Validator.kt
+++ src/main/kotlin/racingcar/util/Validator.kt
@@ -2,6 +2,7 @@ package racingcar.util
 
 import racingcar.util.Constants.ARRAY_SEPARATOR
 import racingcar.util.Constants.EMPTY_ERROR_MESSAGE
+import racingcar.util.Constants.FORMAT_ERROR_MESSAGE
 import racingcar.util.Constants.LENGTH_ERROR_MESSAGE
 import racingcar.util.Constants.MAX_LENGTH
 import racingcar.util.Constants.MIN_LENGTH
@@ -10,25 +11,36 @@ object Validator {
     fun nameValidate(input: String): List<String> {
         nullValidate(input)
         val names = stringSeparator(input)
+            .map { it.trim() }
 
-        for (name in names) {
+        names.forEach { name ->
             nullValidate(name)
             lengthValidate(name)
         }
         return names
     }
 
+    fun attemptalidator(input: String): Int {
+        nullValidate(input)
+        val attemptCount = input.toIntOrNull()
+            ?: throw IllegalArgumentException(FORMAT_ERROR_MESSAGE)
+        numberValidate(attemptCount)
+        return attemptCount
+    }
+
     private fun nullValidate(input: String) {
-        require(input.isNotEmpty() || input.isNotBlank()) { IllegalArgumentException(EMPTY_ERROR_MESSAGE) }
+        require(input.isNotBlank()) { EMPTY_ERROR_MESSAGE }
     }
 
     private fun lengthValidate(input: String) {
-        require(input.length in MIN_LENGTH..MAX_LENGTH) {
-            IllegalArgumentException(LENGTH_ERROR_MESSAGE)
-        }
+        require(input.length in MIN_LENGTH..MAX_LENGTH) { LENGTH_ERROR_MESSAGE }
     }
 
     private fun stringSeparator(input: String): List<String> {
         return input.split(ARRAY_SEPARATOR)
+    }
+
+    private fun numberValidate(input: Int) {
+        require(input >= MIN_LENGTH) { FORMAT_ERROR_MESSAGE }
     }
 }
