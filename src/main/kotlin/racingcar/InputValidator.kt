60c13d810c2df339e59c066a6c5c8c7335f2c100
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-23 00:44:07 +0900

    refactor(InputValidator.kt): 매직넘버 상수화

--- src/main/kotlin/racingcar/InputValidator.kt
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -6,7 +6,11 @@ class InputValidator {
         for (carName in carNames) {
             require(carName.isEmpty()) { "자동차의 이름을 입력하셔야 합니다" }
             require(carName.isNotBlank()) { "자동차의 이름은 빈칸일 수 없습니다" }
-            require(carName.length > 5) { "자동차의 이름은 5글자 이하여야 합니다" }
+            require(carName.length > MAX_CAR_NAME_LENGTH) { "자동차의 이름은 5글자 이하여야 합니다" }
         }
+    }
+
+    companion object {
+        const val MAX_CAR_NAME_LENGTH = 5
     }
 }
