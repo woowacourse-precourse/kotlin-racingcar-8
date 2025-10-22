188d828167a379434b65e78e6759cd231d19bc1c
joon0447 <joon0447@naver.com>
2025-10-22 15:58:37 +0900

    test(carnameparser, carnamevalidator) : parser, validator 분리에 따른 테스트 코드 수정

--- src/test/kotlin/racingcar/model/CarNameParserTest.kt
+++ src/test/kotlin/racingcar/model/CarNameParserTest.kt
@@ -9,27 +9,7 @@ import org.junit.jupiter.api.assertDoesNotThrow
 class CarNameParserTest {
 
     @Test
-    fun `자동차 이름 파싱하기` () {
-        val parser = CarNameParser()
-        assertEquals(listOf("pobi","woni","jun"), parser.parseCarsNames("pobi,woni,jun"))
+    fun `자동차 이름 파싱하기`() {
+        assertEquals(listOf("pobi", "woni", "jun"), CarNameParser.parseCarsNames("pobi,woni,jun"))
     }
-
-    @Test
-    fun `자동차 이름 길이 검증 통과` () {
-        val parser = CarNameParser()
-        assertDoesNotThrow {
-            parser.validateCarName(listOf("pobi","woni","jun"))
-        }
-    }
-
-    @Test
-    fun `자동차 이름 길이 검증 실패` () {
-        val parser = CarNameParser()
-        assertThrows(IllegalArgumentException::class.java) {
-            parser.validateCarName(listOf("abcsdwdds", "ace", "oddvvvd"))
-        }
-    }
-
-
-
 }
---
+++ src/test/kotlin/racingcar/model/CarNameValidatorTest.kt
@@ -0,0 +1,21 @@
+package racingcar.model
+
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class CarNameValidatorTest {
+    @Test
+    fun `자동차 이름 길이 검증 통과`() {
+        assertDoesNotThrow {
+            CarNameValidator.validateCarName(listOf("pobi", "woni", "jun"))
+        }
+    }
+
+    @Test
+    fun `자동차 이름 길이 검증 실패`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            CarNameValidator.validateCarName(listOf("abcsdwdds", "ace", "oddvvvd"))
+        }
+    }
+}
