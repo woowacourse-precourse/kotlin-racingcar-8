dde0d83aaff870155cbeb68ea55931a777c302ae
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-22 23:14:05 +0900

    test(InputViewTest.kt): 테스트 함수 명 수정

--- src/test/kotlin/racingcar/view/InputViewTest.kt
+++ src/test/kotlin/racingcar/view/InputViewTest.kt
@@ -6,7 +6,7 @@ import java.io.ByteArrayInputStream
 
 class InputViewTest {
     @Test
-    fun `입력 받은 값이 문자열 형태로 반환되는지 확인한다`() {
+    fun `입력 받은 자동차 이름이 문자열 형태로 반환되는지 확인한다`() {
         // given
         val fakeInput = "Hello World"
         val inputStream = ByteArrayInputStream(fakeInput.toByteArray())
