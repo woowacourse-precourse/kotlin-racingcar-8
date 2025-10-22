ad568bb6df3051603bee323a573f315f0d33696c
apdlwjwjwj <apdlwjwjwj@gmail.com>
2025-10-22 20:44:47 +0900

    feat: validate car names
    Add validation logic to ensure car names do not exceed 5 characters and handle error messages accordingly.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,19 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = Console.readLine()
+    val names = parse(input)
 }
+
+fun parse(input: String): List<String>{
+    val names = input.split(",").map { it.trim() }
+    if(names.isEmpty()) throw IllegalArgumentException("No name")
+    if(names.any { it.isEmpty() }) throw IllegalArgumentException("Empty name")
+    if(names.any { it.length > 5}) throw IllegalArgumentException("Names can be up to 5 characters long.")
+    return names
+}
+
+
