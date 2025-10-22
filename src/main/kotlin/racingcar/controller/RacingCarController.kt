8fb18dc8d4f95c8d462cf79f97bbe16ba94c869e
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-22 17:16:52 +0900

    refactor: 전체적인 Controller와 View를 관리하는 RacingCarController를 생성함

---
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -0,0 +1,24 @@
+package racingcar.controller
+
+import racingcar.view.View
+
+class RacingCarController {
+    val view = View()
+    val inputController = InputController()
+
+    fun run() {
+        val nameList = view.inputName() // 이름 입력
+        val carList = inputController.createCarList(nameList) // 자동차 리스트
+        val carController = CarController(carList)
+
+        val moves = view.inputCount()// 움직임 값 입력
+        val count = inputController.countToInt(moves) // 움직임 값
+
+        view.movingResult() // 실행 결과
+        repeat(count) {
+            view.currentMoving(carController.currentMoving())
+        }
+
+        view.result(carController.winner())
+    }
+}
