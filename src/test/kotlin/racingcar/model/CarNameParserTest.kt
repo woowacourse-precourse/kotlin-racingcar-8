8b862bb5fba54a75bf9105b24e17b77afaee010e
joon0447 <joon0447@naver.com>
2025-10-22 13:11:23 +0900

    test(carnameparser) : 자동차 이름 길이 검증 테스트 코드 추가

--- src/test/kotlin/racingcar/model/CarNameParserTest.kt
+++ src/test/kotlin/racingcar/model/CarNameParserTest.kt
@@ -1,7 +1,10 @@
 package racingcar.model
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class CarNameParserTest {
 
@@ -10,5 +13,23 @@ class CarNameParserTest {
         val parser = CarNameParser()
         assertEquals(listOf("pobi","woni","jun"), parser.parseCarsNames("pobi,woni,jun"))
     }
+
+    @Test
+    fun `자동차 이름 길이 검증 통과` () {
+        val parser = CarNameParser()
+        assertDoesNotThrow {
+            parser.validateCarName(listOf("pobi","woni","jun"))
+        }
+    }
+
+    @Test
+    fun `자동차 이름 길이 검증 실패` () {
+        val parser = CarNameParser()
+        assertThrows(IllegalArgumentException::class.java) {
+            parser.validateCarName(listOf("abcsdwdds", "ace", "oddvvvd"))
+        }
+    }
+
+
 
 }
