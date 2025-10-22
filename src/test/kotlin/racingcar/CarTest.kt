54d16f6eb443b2d9c7b8b45aac62615f2716a6ee
jsy002514 <jsy002514@naver.com>
2025-10-23 03:48:22 +0900

    feat(cartest):Car 객체 테스트 기능 구현

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,22 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CarTest {
+
+    @Test
+    @DisplayName("무작위 숫자가 4 이상이면 자동차는 전진한다.")
+    fun moveForwardWhenFourMore(){
+        val car = Car("pobi")
+        val randomNumber = 4
+
+        car.moveForward(randomNumber)
+
+        assertThat (car.position).isEqualTo(1)
+    }
+
+    @Test
+    @DisplayName("무작위 숫자가 3 이하이면 자동차는 멈춘다.")
+}
