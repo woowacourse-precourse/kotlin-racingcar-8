318266bb088773ed4acf31dcba409ebecd5fd3e9
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-22 17:17:25 +0900

    refactor: fun()의 실행을 단순화함

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,17 +1,6 @@
 package racingcar
-import camp.nextstep.edu.missionutils.Console as Console
+import racingcar.controller.RacingCarController
 
 fun main() {
-    val inputController = InputController()
-
-    View.inputName() // 이름 입력
-    val carList = inputController.createCarList(Console.readLine()) // 자동차 리스트
-
-    View.inputCount() // 움직임 값 입력
-    val count = inputController.countToInt(Console.readLine()) // 움직임 값
-
-    val carController = CarController(carList) // 자동차 컨트롤러
-    View.movingResult(carController, count) // 실행 결과
-    View.result(carController) // 최종 우승자
-
+    RacingCarController().run()
 }
