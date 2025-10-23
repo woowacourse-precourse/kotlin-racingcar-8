a2f2ce9ae805d3804a3b4ce3360bccbe83bdaa8a
cbnuLeehyunwoo <henow123@naver.com>
2025-10-24 03:35:13 +0900

    feat(Application.kt): 자동차 이름을 입력받고 파싱하는 기능 구현
    문자열을 입력받고 콤마를 기준으로 나누어 문자열 리스트를 반환하는
    getCarNamesList() 함수를 구현했습니다. 입력은 Kotline의 기본 readLine() 대신
    camp.nextstep.edu.missionutils.Console.readLine()을 사용하여 입력을
    처리했습니다. 코틀린의 기본 입력 대신 해당 메서드를 사용하는 이유는 다음으로 생각됩니다.
    
    1. 해당 메서드는 내부에서 자바의 Scanner(System.in))을 이용해 입력을 받고,
    스캐너가 닫혀있으면 새로 생성하여 입력 스트립을 안정적으로 유지합니다.
    2. 입력을 모킹하는 로직이 설계되어있어 테스트 환경에서의 검증을 용이하게
    해줍니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
+fun getCarNamesList(): List<String> {
+    val carNamesList = readLine().split(",")
+    return carNamesList
+}
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNamesList = getCarNamesList()
 }
