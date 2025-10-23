05f734dcc456d2e03877d9662030043cc67a93ff
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-10-24 04:04:01 +0900

    feat(input): input 메서드 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,18 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+}
+
+fun infoMessage() {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+}
+
+fun input(): String? {
+    return Console.readLine()
+}
+
+fun output() {
 }
