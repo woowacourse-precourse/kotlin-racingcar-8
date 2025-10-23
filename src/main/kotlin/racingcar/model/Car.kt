3059e207bc1ca4ed0f070da7aefafaffcb4fa82d
joon0447 <joon0447@naver.com>
2025-10-23 21:59:22 +0900

    refactor(car) : data class로 변경

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -2,7 +2,7 @@ package racingcar.model
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class Car(val name: String, var position: Int = 0) {
+data class Car(val name: String, var position: Int = 0) {
 
     fun move() {
         val random = Randoms.pickNumberInRange(0, 9)
