e90a8706c8a62525b6bb37c85e42b751deab6f3f
joon0447 <joon0447@naver.com>
2025-10-23 22:22:49 +0900

    test(carstest) : 레이싱 진행 테스트 코드 추가

--- src/test/kotlin/racingcar/model/CarsTest.kt
+++ src/test/kotlin/racingcar/model/CarsTest.kt
@@ -1,6 +1,7 @@
 package racingcar.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class CarsTest {
@@ -9,7 +10,20 @@ class CarsTest {
     fun `리스트로 Car 생성하기`() {
         val carNames = mutableListOf<String>("A", "B", "C")
         val cars = mutableListOf<Car>(Car("A"), Car("B"), Car("C"))
-
         assertEquals(cars, Cars().createCar(carNames))
+    }
+
+    @Test
+    fun `레이싱 진행 후 차 위치 확인`() {
+        val cars = Cars()
+        cars.createCar(listOf("A", "B", "C"))
+        cars.startRound()
+
+        val carList = cars.getCars()
+
+        carList.forEach {
+            assertTrue(it.position >= 0)
+            println("${it.name}의 위치 : ${it.position}")
+        }
     }
 }
