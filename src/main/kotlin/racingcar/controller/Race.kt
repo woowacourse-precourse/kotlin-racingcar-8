5389ce3a649484ab64178bfe17ba0c1e81121059
joon0447 <joon0447@naver.com>
2025-10-23 23:14:29 +0900

    feat(race) : 경주 횟수 입력 검증 단계 추가

--- src/main/kotlin/racingcar/controller/Race.kt
+++ src/main/kotlin/racingcar/controller/Race.kt
@@ -2,6 +2,7 @@ package racingcar.controller
 
 import racingcar.model.CarNameParser
 import racingcar.model.CarNameValidator
+import racingcar.model.RaceCountValidator
 import racingcar.view.InputView
 
 class Race {
@@ -9,6 +10,7 @@ class Race {
     fun run() {
         val rawCarNames = InputView.readCarNameInput()
         val raceCount = InputView.readRaceCountInput()
+        RaceCountValidator.validateRaceCount(raceCount)
 
         val parseCarNames = CarNameParser.parseCarsNames(rawCarNames)
         CarNameValidator.validateCarName(parseCarNames)
