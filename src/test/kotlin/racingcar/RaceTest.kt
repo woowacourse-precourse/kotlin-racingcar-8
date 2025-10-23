e8c0c606a4fa3d0448e532883e230e2ad7d7b2a1
baekcii <bsy8470@naver.com>
2025-10-23 23:32:45 +0900

    test(Race): 랜덤값이 4 이상인 참가자만 전진하는 테스트 추가

---
+++ src/test/kotlin/racingcar/RaceTest.kt
@@ -0,0 +1,33 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import racingcar.model.User
+
+class RaceTest : NsTest() {
+
+    @Test
+    fun `랜덤값이 4 이상인 참가자만 전진`() {
+        assertRandomNumberInRangeTest(
+            {
+                val users = listOf(User("a"), User("b"))
+                val race = Race(users)
+
+                race.play()
+                assertThat(users[0].score).isEqualTo(1)
+                assertThat(users[1].score).isEqualTo(0)
+            }, MOVING_FORWARD, STOP
+        )
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val MOVING_FORWARD: Int = 4
+        private const val STOP: Int = 3
+    }
+}
