cc7c215163f0359e8de076d3d2c1e304c7d36cb5
cbnuLeehyunwoo <henow123@naver.com>
2025-10-24 03:38:01 +0900

    feat(Application.kt): 시도 횟수 입력 기능 추가
    시도 횟수를 입력받아, 반환하는 getRepeatNum() 함수를 구현헀습니다.
    우선은 Int형으로 변환할 수 없는 입력에 대해서는 0의 기본값을 부여하도록
    하였습니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,7 +7,14 @@ fun getCarNamesList(): List<String> {
     return carNamesList
 }
 
+fun getRepeatNum(): Int {
+    val repeatNum = readLine().toIntOrNull()?: 0
+    return repeatNum
+}
+
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carNamesList = getCarNamesList()
+    println("시도할 횟수는 몇 회인가요?")
+    val repeatNum = getRepeatNum()
 }
