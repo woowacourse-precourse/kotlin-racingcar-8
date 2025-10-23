6c83c01edd27ab0d10408b8a2b754647eb4cda2c
joon0447 <joon0447@naver.com>
2025-10-23 23:55:32 +0900

    feat(race) : 실행 결과 메시지, 라운드별 진행 상황 출력

--- src/main/kotlin/racingcar/controller/Race.kt
+++ src/main/kotlin/racingcar/controller/Race.kt
@@ -2,17 +2,27 @@ package racingcar.controller
 
 import racingcar.model.CarNameParser
 import racingcar.model.CarNameValidator
+import racingcar.model.Cars
 import racingcar.model.RaceCountValidator
 import racingcar.view.InputView
+import racingcar.view.OutputView
 
 class Race {
 
     fun run() {
+        val cars = Cars()
         val rawCarNames = InputView.readCarNameInput()
         val raceCount = InputView.readRaceCountInput()
         RaceCountValidator.validateRaceCount(raceCount)
 
         val parseCarNames = CarNameParser.parseCarsNames(rawCarNames)
         CarNameValidator.validateCarName(parseCarNames)
+
+        cars.createCar(parseCarNames)
+        OutputView.printStartRacing()
+        repeat(raceCount.toInt(), {
+            cars.startRound()
+            OutputView.printPerRound(cars.getCars())
+        })
     }
 }
