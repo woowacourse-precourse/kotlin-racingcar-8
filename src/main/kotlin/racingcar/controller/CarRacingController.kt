938557f0e2984ebd2467a23b55cbfbd5820c338c
Jo Eungyeong <joeungyeong23@gmail.com>
2025-10-24 05:54:35 +0900

    feat(input): 자동차 이름 읽어오는 기능 추가

--- src/main/kotlin/racingcar/controller/CarRacingController.kt
+++ src/main/kotlin/racingcar/controller/CarRacingController.kt
@@ -1,9 +1,11 @@
 package racingcar.controller
 
+import racingcar.view.InputView
 import racingcar.view.OutputView
 
 class CarRacingController {
     fun run() {
         OutputView.displayCarNamePrompt()
+        val input = InputView.readCarNames()
     }
 }
---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,9 @@
+package racingcar.view
+
+import camp.nextstep.edu.missionutils.Console
+
+object InputView {
+    fun readCarNames(): String {
+        return Console.readLine()
+    }
+}
