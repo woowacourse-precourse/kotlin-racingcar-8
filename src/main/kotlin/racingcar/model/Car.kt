93f5b9e54b045f065249234fad6b2409d5d9c3fc
joon0447 <joon0447@naver.com>
2025-10-23 21:26:29 +0900

    feat(car) : car 클래스 추가, 차량 이동 함수 구현

---
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -0,0 +1,11 @@
+package racingcar.model
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Car(val name: String, var position: Int = 0) {
+
+    fun move() {
+        val random = Randoms.pickNumberInRange(0, 9)
+        if (random >= 4) position++
+    }
+}
