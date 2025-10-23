a9d4ac3910218078d0b509528c89d6c162302cb7
jangdaehan <wkdeogks17@gmail.com>
2025-10-23 20:07:59 +0900

    feat(car): add car class to save name
    check minimum name length in initializer block to meet requirement.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,11 @@
 package racingcar
 
+data class Car(val name: String) {
+    init {
+        require(name.isNotEmpty()) { "자동차 이름은 한 글자 이상을 포함해야 합니다." }
+    }
+}
+
 fun main() {
     // TODO: 프로그램 구현
 }
