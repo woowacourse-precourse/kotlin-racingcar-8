900aec5a44748b9fb77a5c33e523bb75ae0851c2
cbnuLeehyunwoo <henow123@naver.com>
2025-10-24 03:52:13 +0900

    feat(Application.kt): 자동차 이름 입력을 검증하고 예외처리하는 기능 구현
    사용자가 입력한 자동차 이름을 검증하는 기능을 validateCarNames() 함수에
    구현하였습니다.
    
    - 빈 문자열이거나, 5글자를 초과하는 이름을 예외처리 대상으로
    설정했습니다.
    - 코틀린의 예외처리 함수 require()를 사용했습니다
    - 해당 함수의 매개변수가 false로 평가되면 IllegalArgumentException을
    발생시킵니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,13 +4,24 @@ import camp.nextstep.edu.missionutils.Console.readLine
 
 fun getCarNamesList(): List<String> {
     val carNamesList = readLine().split(",")
+    validateCarNames(carNamesList)
     return carNamesList
 }
 
 fun getRepeatNum(): Int {
-    val repeatNum = readLine().toIntOrNull()?: 0
-    return repeatNum
+    val repeatNum = readLine()
+    return repeatNum.toInt()
 }
+
+
+fun validateCarNames(carNames: List<String>): Unit {
+    require(carNames.isNotEmpty()) {"최소 한 대 이상의 자동차 이름이 필요합니다."}
+    carNames.forEach {
+        require(it.isNotBlank()) {"자동차 이름은 빈 문자열일 수 없습니다."}
+        require(it.length <= 5) {"자동차 이름은 5글자를 초과할 수 없습니다."}
+    }
+}
+
 
 fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
