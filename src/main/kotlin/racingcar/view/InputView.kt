047c46a6691f5994c8ba22905f0d36dab8646b63
baekcii <bsy8470@naver.com>
2025-10-24 14:47:03 +0900

    feat(input): InputView 구현 및 OutputMessage로 상수화

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,10 +1,17 @@
 package racingcar.view
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.constant.InputMessage
 
 object InputView {
 
-    fun readParticipants(): String {
+    private fun prompt(message: InputMessage): String {
+        println(message.text)
         return Console.readLine() ?: ""
     }
+
+    fun readParticipants(): String = prompt(InputMessage.NAME)
+
+    fun readRound(): String = prompt(InputMessage.ROUND)
+
 }
