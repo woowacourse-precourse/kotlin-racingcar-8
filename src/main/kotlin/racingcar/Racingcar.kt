e64c39393428d6c93ff9472484ce71fe915c3643
linux_csy <tnehd83@naver.com>
2025-10-24 18:41:08 +0900

    feat(racingcar): 레이싱 게임 실행 흐름 구현

--- src/main/kotlin/racingcar/Racingcar.kt
+++ src/main/kotlin/racingcar/Racingcar.kt
@@ -1,7 +1,25 @@
 package racingcar
 
+import racingcar.domain.Car
+import racingcar.domain.RacingGame
+import racingcar.util.CarNameParser
+import racingcar.util.Validator
+import racingcar.view.ConsoleView
+
 class Racingcar {
     fun run() {
+        val carNamesInput = ConsoleView.inputCarNames()
 
+        val carNames = CarNameParser.parse(carNamesInput)
+        Validator.validateCarNames(carNames)
+        val cars = carNames.map { Car(it) }
+
+        val rounds = ConsoleView.inputTryCount()
+        Validator.validateRoundCheck(rounds)
+
+        val racingGame = RacingGame(cars)
+        racingGame.play(rounds)
+
+        ConsoleView.showRaceWinner(racingGame)
     }
 }
