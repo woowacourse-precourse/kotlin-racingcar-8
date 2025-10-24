3dabc1a61ba5baac16a88761dc7b872f6b6f764e
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-24 15:54:12 +0900

    feat(RacingController.kt): 레이싱 로직 흐름 작성

--- src/main/kotlin/racingcar/RacingController.kt
+++ src/main/kotlin/racingcar/RacingController.kt
@@ -1,18 +1,42 @@
 package racingcar
 
+import racingcar.domain.Car
 import racingcar.view.InputView
 
-class RacingController {
+class RacingController(val inputView: InputView, outputView: OutputView) {
     fun run() {
-        val carName = InputView().getCarNameFromUser()
+        val carName = inputView.getCarNameFromUser()
         val carNames = Separator().separateName(carName)
         InputValidator().validateCarName(carNames)
 
-        val repeatTime = InputView().getMovementTimeFromUser()
+        val repeatTime = inputView.getMovementTimeFromUser()
         InputValidator().validateTotalMovement(repeatTime)
+
+        val raceResult = race(carNames, repeatTime.toInt())
+        judgeWinner()
+        printWinner()
     }
 
-    fun race() {
+    fun race(carNames: List<String>, repeatTime: Int): List<Car> {
+        val cars: List<Car> = carNames.map(::Car)
 
+        for(i in 0 until repeatTime) {
+            moveCar(cars)
+            printCurrentCarState(cars)
+        }
+        return cars
+    }
+
+    private fun moveCar(cars: List<Car>) {
+        for(car in cars) {
+            car.moveForward()
+        }
+    }
+
+    private fun printCurrentCarState(cars: List<Car>) {
+        for(car in cars) {
+            outputView.printCurrentLocation(car.name, car.distance)
+        }
+        println()
     }
 }
