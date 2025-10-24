9980c07bc1d76340f925ab567523b115cc342034
Jo Eungyeong <joeungyeong23@gmail.com>
2025-10-24 21:09:16 +0900

    refactor(test): car 패키지 안으로 이동

--- src/test/kotlin/racingcar/service/car/CarServiceTest.kt
+++ src/test/kotlin/racingcar/service/car/CarServiceTest.kt
@@ -1,10 +1,9 @@
-package racingcar.service
+package racingcar.service.car
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.DisplayNameGenerator
 import org.junit.jupiter.api.Test
-import racingcar.service.car.CarService
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
 class CarServiceTest {
@@ -20,9 +19,9 @@ class CarServiceTest {
         val cars = carService.createCars(input)
 
         // then
-        assertEquals(cars.cars.size, 3)
-        assertEquals(cars.cars[0].name.value, "pobi")
-        assertEquals(cars.cars[1].name.value, "woni")
-        assertEquals(cars.cars[2].name.value, "jun")
+        Assertions.assertEquals(cars.cars.size, 3)
+        Assertions.assertEquals(cars.cars[0].name.value, "pobi")
+        Assertions.assertEquals(cars.cars[1].name.value, "woni")
+        Assertions.assertEquals(cars.cars[2].name.value, "jun")
     }
 }
