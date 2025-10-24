92a18459bf9d19cf9be2950a26d8a2fbbad2e5df
its10623 <its10623@gmail.com>
2025-10-24 12:03:56 +0900

    feat: Object 싱글턴 객체 생성

---
+++ src/main/kotlin/racingcar/RacingCarConstants.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+object RacingCarConstants {
+    const val CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"
+    const val COUNT_OF_ATTEMPT_MESSAGE = "시도할 횟수는 몇 회인가요?"
+    const val EXECUTION_RESULT_MESSAGE = "실행 결과"
+    const val VALIDATION_MESSAGE = "잘못된 값이 입력 되었습니다."
+
+    const val MOVE_NUMBER = 4
+
+}
