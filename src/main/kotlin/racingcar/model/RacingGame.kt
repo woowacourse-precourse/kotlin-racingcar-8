5db27cccdd28c2365d2da3272c9b021b1a4a2d41
friendshipHyun <commit3921@gmail.com>
2025-10-24 10:36:42 +0900

    feat: 시도 검증 추가

--- src/main/kotlin/racingcar/model/RacingGame.kt
+++ src/main/kotlin/racingcar/model/RacingGame.kt
@@ -3,6 +3,11 @@ package racingcar.model
 class RacingGame(private val carNames: List<String>, private val tries: String) {
     private val cars = carNames.map { Car(it) }
 
+    init {
+        require(tries.toIntOrNull() != null) { "정수가 아닌 입력값입니다." }
+        require(tries.toInt() != 0) { "횟수는 0보다 큰 정수이여야 합니다." }
+    }
+
     fun playRound(): List<Car> {
         cars.forEach { it.move() }
         return cars
