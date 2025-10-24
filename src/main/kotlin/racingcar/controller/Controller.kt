61cdcb624763148385df7c2697bab67d89ef21f6
baekcii <bsy8470@naver.com>
2025-10-24 15:16:50 +0900

    feat(controller): view, model 연결 및 전체 실행 흐름 구현

--- src/main/kotlin/racingcar/controller/Controller.kt
+++ src/main/kotlin/racingcar/controller/Controller.kt
@@ -1,13 +1,29 @@
 package racingcar.controller
 
+import racingcar.model.Race
+import racingcar.model.UserParser
 import racingcar.validator.Validator
 import racingcar.view.InputView
+import racingcar.view.OutputView
 
 class Controller {
     fun run() {
-
         val inputName = InputView.readParticipants()
         Validator.validateNotBlank(inputName)
-    }
 
+        val userParser = UserParser(inputName)
+
+        val inputRound = InputView.readRound()
+        Validator.validateRoundInput(inputRound)
+        val round = inputRound.toInt()
+
+        val race = Race(userParser.getUserList())
+
+        OutputView.displayTitle()
+        repeat(round) {
+            race.play()
+            OutputView.displayRoundResult(race.getScores())
+        }
+        OutputView.displayWinners(race.getWinners())
+    }
 }
