d2d910fc8ee5668d4bb2d17a0343fe960e7d1e51
jangdaehan <wkdeogks17@gmail.com>
2025-10-24 18:52:30 +0900

    feat(car): implement moving feature to car

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,20 @@
 package racingcar
 
-data class Car(val name: String) {
+data class Car(val name: String, val position: Int) {
     init {
         require(name.isNotEmpty()) { "자동차 이름은 한 글자 이상을 포함해야 합니다." }
         require(name.length <= 5) { "자동차 이름은 5글자 이하여야 합니다." }
     }
+
+    init {
+        require(position >= 0) { "자동차의 위치($position)는 0미만일 수 없습니다." }
+    }
+
+    fun moveForward(): Car {
+        return Car(name, position + 1)
+    }
+
+    constructor(name: String) : this(name, 0)
 }
 
 fun createUniqueCars(names: List<String>): List<Car> {
