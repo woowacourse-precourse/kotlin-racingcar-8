40055bc1f1f60dce49db13d89d59df9b8a5c9b53
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-10-24 16:38:14 +0900

    fix(processCarNaming): 자동차 이름 분류 함수 수정
    input을 받아 쉼표로 분류하여 리스트로 만듬

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,21 +4,22 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     // TODO: 프로그램 구현
-    print(processCarNaming("pobi,zz"))
+    print(processCarNaming())
+
 }
 
 fun infoMessage() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
 }
 
-fun input(): String? {
+fun input(): String {
     return Console.readLine()
 }
 
 fun output() {
 }
 
-fun processCarNaming(input: String): String {
-    val carNames = listOf(input)
-    return carNames.joinToString(",")
+fun processCarNaming(): List<String> {
+    val carNames = input().split(",")
+    return listOf(carNames.joinToString(","))
 }
