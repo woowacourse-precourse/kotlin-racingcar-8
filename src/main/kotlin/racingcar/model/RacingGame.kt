3d233dbc1aee6c07880718bb255a25fd00136b49
cbnuLeehyunwoo <henow123@naver.com>
2025-10-26 02:02:19 +0900

    refatcor(model): RacingGame 클래스의 불필요한 import 및 상수 제거
    난수 생성 책임이 NumberGenerator로 이전되어, RacingGame 내부의 import문 및
    MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER 상수가 더 이상 사용되지 않아 제거하였습니다.

--- src/main/kotlin/racingcar/model/RacingGame.kt
+++ src/main/kotlin/racingcar/model/RacingGame.kt
@@ -1,10 +1,4 @@
 package racingcar.model
-import camp.nextstep.edu.missionutils.Randoms
-import racingcar.view.OutputView
-
-private const val MIN_RANDOM_NUMBER = 0
-private const val MAX_RANDOM_NUMBER = 9
-
 class RacingGame(private val numberGenerator: NumberGenerator) {
     fun raceOneRound(cars: List<Car>): List<Car> {
         return cars.map { it. move(numberGenerator)}
