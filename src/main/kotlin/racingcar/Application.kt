3d62278581adb34545576de11451d3cc7fa2fda0
First_woosun <dbdntjs620@gmail.com>
2025-10-25 17:00:37 +0900

    fix: 객체 생성 버그 수정
    생성자와 필드가 충종하는 현상 수정
    
    feat: main함수 작성
    
    이름과 수행 횟수를 입력받아 경주를 수행하는 기능 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,27 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    // TODO: 프로그램 구현
+    // 입력 화면 출력
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    var racerNames: String = Console.readLine()
+    println("시도할 횟수는 몇 회인가요?")
+    var attemptNumbers: Int = Console.readLine().toInt()
+
+    // 레이서 객체 생성
+    var extractedRacerNames = extractRacerName(racerNames)
+    var racerCollection: List<Racer> = extractedRacerNames.map { Racer(it) }
+
+    //경기 수행
+    println("실행 결과")
+    for(i in 1..attemptNumbers){
+        for(racer in racerCollection){
+            racer.doRandomProgress()
+        }
+        for(racer in racerCollection){
+            println(racer.getLastDistance())
+        }
+        println()
+    }
 }
 
 // 입력받은 문자열에서 이름들을 추출하는 함수
@@ -15,7 +35,8 @@ fun extractRacerName(inputNameString: String): List<String> {
     return returnCollection
 }
 
-public class Racer(var name: String){
+public class Racer(var nameInput: String){
+    private var name = nameInput
     private var distance = StringBuilder()
     private var distanceNumber: Int = 0
 
