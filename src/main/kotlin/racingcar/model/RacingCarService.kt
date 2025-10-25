f2dbff6d4d4cef0cd9d0e8baa6e713386b374f0b
byunghyunkim0 <bb05qudgus@gamil.com>
2025-10-25 23:21:01 +0900

    feat: getWinPlayers 기능 구현

--- src/main/kotlin/racingcar/model/RacingCarService.kt
+++ src/main/kotlin/racingcar/model/RacingCarService.kt
@@ -8,4 +8,10 @@ class RacingCarService {
     fun playRace(players: List<Car>) {
         players.forEach { it.move() }
     }
+
+    fun getWinPlayers(players: List<Car>): List<String> {
+        val maxPosition = players.maxOfOrNull { it.position }
+
+        return players.filter { it.position == maxPosition }.map { it.name }
+    }
 }
