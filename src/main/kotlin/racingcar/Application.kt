27584fd54e62ecdf8c43cc2ba320e6e31458d84e
Soldbone <1472ezs@gmail.com>
2025-10-25 23:10:04 +0900

    refactor: 입력 및 입력 처리 기능 함수화
    다음과 같은 이름으로 함수화:
    - getInputCarName(): 자동차 이름 입력 및 예외처리
    - splitCarName(name): 입력 받은 이름에서 각 자동차 이름 추출 및 예외처리
    - getInputRound(): 차수 입력 및 예외처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,29 +6,46 @@ fun main() {
     // TODO: 프로그램 구현
 
     // 입력
-    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    val carNamesInput = Console.readLine()
-    val hasNonCommaSpecialChar = carNamesInput.any { it != ',' && !it.isLetter() }
-    require(!hasNonCommaSpecialChar) { "이름을 올바르게 입력해주세요." }
+    val carNamesInput = getInputCarNames()
+    val splitCarNames = splitCarName(carNamesInput)
 
-    require(carNamesInput.isNotBlank()) { "이름이 입력되지 않았습니다." }
+    val round = getInputRound()
 
-    val splitCarName = carNamesInput.split(",")
-    val hasLengthFiveOrLess = splitCarName.all { it.length <= 5 && it.isNotBlank() }
-    require(hasLengthFiveOrLess) { "이름은 5글자를 초과하거나 비어있을 수 없습니다." }
-
-    println("시도할 횟수는 몇 회인가요?")
-    val roundInput = Console.readLine()
-    val round = stringToInt(roundInput)
-
-    val carInstances = splitCarName.map { Car(it) }
+    val carInstances = splitCarNames.map { Car(it) }
     val racingGame = RacingGame(round, carInstances)
 
 }
 
+private fun getInputCarNames(): String {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val carNamesInput = Console.readLine()
+    val hasNonCommaSpecialChar = carNamesInput.any { it != ',' && !it.isLetter() }
+
+    require(!hasNonCommaSpecialChar) { "이름을 올바르게 입력해주세요." }
+    require(carNamesInput.isNotBlank()) { "이름이 입력되지 않았습니다." }
+
+    return carNamesInput
+}
+
+private fun splitCarName(carNamesInput: String): List<String> {
+    val splitCarNames = carNamesInput.split(",")
+    val hasLengthFiveOrLess = splitCarNames.all { it.length <= 5 && it.isNotBlank() }
+    require(hasLengthFiveOrLess) { "이름은 5글자를 초과하거나 비어있을 수 없습니다." }
+
+    return splitCarNames
+}
+
+private fun getInputRound(): Int {
+    println("시도할 횟수는 몇 회인가요?")
+    val roundInput = Console.readLine()
+    val round = stringToInt(roundInput)
+
+    return round
+}
+
 private fun stringToInt(roundInput: String): Int =
     if (roundInput.isNotBlank()) {
-    roundInput.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
+        roundInput.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
     } else {
         0
     }
