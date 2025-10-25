79f71d97e51aa9a6f27229a759873e56fb836a2a
joon0447 <joon0447@naver.com>
2025-10-25 14:52:30 +0900

    feat(carnamevalidator) : 자동차 이름 검증 조건 추가

--- src/main/kotlin/racingcar/model/CarNameValidator.kt
+++ src/main/kotlin/racingcar/model/CarNameValidator.kt
@@ -1,9 +1,11 @@
 package racingcar.model
 
 object CarNameValidator {
+
     fun validateCarName(carNames: List<String>) {
         carNames.forEach { name ->
-            require(name.length <= 5)
+            require(name.length in 1..5) { "각 자동차의 이름의 길이는 1~5자 이어야 합니다."}
         }
     }
+
 }
