d0cd272b4c4378bed5f323c753301803b733f811
JCTA0125 <jcta0125@duksung.ac.kr>
2025-10-25 20:54:06 +0900

    feat(CarValidator): 이름 조건 확인

--- src/main/kotlin/racingcar/model/CarValidator.kt
+++ src/main/kotlin/racingcar/model/CarValidator.kt
@@ -3,7 +3,14 @@ package racingcar.model
 class CarValidator {
     fun parseAndValidateCarName(input: String): List<String> {
         val names = input.split(",").map { it.trim() }
+        validateCarName(names)
 
         return names
+    }
+
+    private fun validateCarName(names: List<String>) {
+        require(names.all { it.isNotBlank() }) { throw IllegalArgumentException("자동차 이름은 비어 있을 수 없습니다." ) }
+        require(names.all { it.length <= 5 }) { throw IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.") }
+        require(names.distinct().size == names.size) { throw IllegalArgumentException("자동차 이름은 중복될 수 없습니다.") }
     }
 }
