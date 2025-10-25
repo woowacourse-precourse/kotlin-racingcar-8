6e52dc27385c9014201cff92f4c26b0c4f12c25d
cbnuLeehyunwoo <henow123@naver.com>
2025-10-26 01:46:02 +0900

    refactor(model): Validator 클래스 상수 정의 위치를 클래스 내부로 이동
    기존에는 Validator.kt 파일 최상단에 상수를 선언하였는데 해당 방법은
    Validator의 테스트 코드에서 상수를 재사용 할 수 없을 것이라 생각했습니다.
    
    이에 따라, 상수의 접근성과 재사용성을 높이기 위해 Validator 클래스 내부로
    상수 정의를 이동하였습니다.

--- src/main/kotlin/racingcar/model/Validator.kt
+++ src/main/kotlin/racingcar/model/Validator.kt
@@ -1,14 +1,14 @@
 package racingcar.model
-private const val ERROR_NO_CAR_NAMES = "최소 한 대 이상의 자동차 이름이 필요합니다."
-private const val ERROR_BLANK_CAR_NAME = "자동차 이름은 빈 문자열일 수 없습니다."
-private const val ERROR_CAR_NAME_TOO_LONG = "자동차 이름은 ${Car.MAX_CAR_NAME_LENGTH}글자를 초과할 수 없습니다."
-private const val ERROR_EMPTY_TRIAL_COUNT = "시도 횟수는 빈 문자열일 수 없습니다."
-private const val ERROR_TRIAL_COUNT_NOT_A_NUMBER = "시도 횟수는 숫자여야 합니다."
-private const val ERROR_TRIAL_COUNT_NOT_POSITIVE = "시도 횟수는 1 이상의 정수여야 합니다."
-private const val ERROR_DUPLICATE_CAR_NAME = "자동차 이름은 중복될 수 없습니다."
-
 
 object Validator {
+    const val ERROR_NO_CAR_NAMES = "최소 한 대 이상의 자동차 이름이 필요합니다."
+    const val ERROR_DUPLICATE_CAR_NAME = "자동차 이름은 중복될 수 없습니다."
+    const val ERROR_CAR_NAME_TOO_LONG = "자동차 이름은 ${Car.MAX_CAR_NAME_LENGTH}글자를 초과할 수 없습니다."
+    const val ERROR_BLANK_CAR_NAME = "자동차 이름은 빈 문자열일 수 없습니다."
+    const val ERROR_EMPTY_TRIAL_COUNT = "시도 횟수는 빈 문자열일 수 없습니다."
+    const val ERROR_TRIAL_COUNT_NOT_A_NUMBER = "시도 횟수는 숫자여야 합니다."
+    const val ERROR_TRIAL_COUNT_NOT_POSITIVE = "시도 횟수는 1 이상의 정수여야 합니다."
+
     fun validateCarNames(carNames: List<String>): Unit {
         require(carNames.isNotEmpty()) { ERROR_NO_CAR_NAMES }
         require(carNames.size == carNames.toSet().size) { ERROR_DUPLICATE_CAR_NAME }
@@ -22,7 +22,4 @@ object Validator {
         val number = repeatNum.toIntOrNull()?: throw IllegalArgumentException( ERROR_TRIAL_COUNT_NOT_A_NUMBER )
         require(number > 0) { ERROR_TRIAL_COUNT_NOT_POSITIVE }
     }
-
-
-
 }
