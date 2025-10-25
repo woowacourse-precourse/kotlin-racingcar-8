6fe9d6ae0704c563940a65ee20cfa8b152f65a0f
umrhiumrhi <umrhiumrhi@naver.com>
2025-10-26 08:16:46 +0900

    feat(racingcar): 자동차 이름 입력 기능 구현
    - 쉼표(,)로 구분된 자동차 이름들을 입력으로 받음
    - 공백 제거 및 빈 문자열은 제외
    - List<String>으로 반환

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,18 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val names = readCarNames()
+}
+
+private fun readCarNames(): List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val input = Console.readLine()
+
+    val names = input.split(",")
+        .map { it.trim() }
+        .filter { it.isNotBlank() }
+
+    return names
 }
