24f22e62c102c2e7f33b76f04f0baa2662ed7d57
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-10-25 20:11:27 +0900

    refactor: View 클래스와 메서드를 open 타입으로 변경함

--- src/main/kotlin/racingcar/view/View.kt
+++ src/main/kotlin/racingcar/view/View.kt
@@ -3,21 +3,21 @@ package racingcar.view
 import camp.nextstep.edu.missionutils.Console
 import racingcar.model.Car
 
-class View {
+open class View {
     // 이름을 받을 때 나타나는 함수
-    fun inputName(): String {
+    open fun inputName(): String {
         println(INPUT_NAMES)
         return Console.readLine()
     }
 
     // 움직임 횟수를 받을 때 나타나는 함수
-    fun inputCount(): String {
+    open fun inputCount(): String {
         println(INPUT_COUNT)
         return Console.readLine()
     }
 
     // 현재까지의 자동차 움직임을 나타내주는 함수
-    fun currentMoving(currentMoving: List<Car>) {
+    open fun currentMoving(currentMoving: List<Car>) {
         for (i in currentMoving) {
             println(i.name + " : " + MOVE_STRING.repeat(i.moves))
         }
@@ -32,7 +32,7 @@ class View {
     }
 
     // 최종 우승자를 나타내는 함수
-    fun result(winnerList: List<String>) {
+    open fun result(winnerList: List<String>) {
         println(OUTPUT_STRING + " : " + winnerList.joinToString(", "))
     }
 
