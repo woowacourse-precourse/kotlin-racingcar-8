cdd00a0ab6de3dd05daddda83a070d812ab37d3d
umrhiumrhi <umrhiumrhi@naver.com>
2025-10-26 08:49:22 +0900

    feat(racingcar): 자동차 이동 기능 추가
    moveCars() 메서드 구현
     - 각 자동차에 대해 0~9 범위의 랜덤 숫자를 생성해서 move() 호출

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -1,7 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Game(private val cars: List<Car>) {
     fun play() {
+        moveCars()
+    }
 
+    private fun moveCars() {
+        cars.forEach { it.move(Randoms.pickNumberInRange(0,9)) }
     }
 }
