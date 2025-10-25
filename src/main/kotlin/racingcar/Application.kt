64fee96665d7cffc1bc31a955b3be34f79101b9a
First_woosun <dbdntjs620@gmail.com>
2025-10-25 16:36:14 +0900

    feat: Racer 객체 추가
    각 필드 및 getter를 포함한 Racer 클래스 추가

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,28 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+}
+
+public class Racer(var name: String){
+    private var distance = StringBuilder()
+    private var distanceNumber: Int = 0
+
+    fun doRandomProgress(){
+        if(Randoms.pickNumberInRange(0, 9) >= 4) {
+            this.distance.append("-")
+            this.distanceNumber += 1
+        }
+    }
+
+    fun getName(): String { return this.name }
+
+    fun getDistance(): String { return this.distance.toString() }
+
+    fun getLastDistance(): String {
+        return this.name+" : "+this.distance.toString()
+    }
 }
