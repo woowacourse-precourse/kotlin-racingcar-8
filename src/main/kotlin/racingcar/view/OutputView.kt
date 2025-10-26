a2bfa749e2d22c21dbc097b88901e9c8551e9d25
its10623 <its10623@gmail.com>
2025-10-26 22:05:25 +0900

    chore: 자동차 이름 입력 메세지 오타 수정

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -5,7 +5,7 @@ import racingcar.model.Car
 class OutputView {
 
     companion object {
-        private const val CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"
+        private const val CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         private const val ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?"
         private const val EXECUTION_RESULT_MESSAGE = "\n실행 결과"
         private const val FINAL_WINNER_MESSAGE = "최종 우승자 : "
