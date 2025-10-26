38fe8cde135b23028160e43be91a9ad2a3ab46c2
its10623 <its10623@gmail.com>
2025-10-26 12:26:14 +0900

    refactor: 가짜 랜덤 번호 생성을 위한 인터페이스 클래스로 수정

--- src/main/kotlin/racingcar/model/RandomNumberGenerator.kt
+++ src/main/kotlin/racingcar/model/RandomNumberGenerator.kt
@@ -2,8 +2,12 @@ package racingcar.model
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class RandomNumberGenerator {
-    fun getRandomNumber(): Int {
+interface NumberGenerator {
+    fun getRandomNumber(): Int
+}
+
+class RandomNumberGenerator: NumberGenerator {
+    override fun getRandomNumber(): Int {
         return Randoms.pickNumberInRange(1, 9)
     }
 }
