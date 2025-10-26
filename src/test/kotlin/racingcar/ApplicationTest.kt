932bfce4919c84ab5e53bfa19f901ca432fbb5f4
jangdaehan <wkdeogks17@gmail.com>
2025-10-26 18:17:37 +0900

    test(car): add test if cars move in the right condition

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -4,11 +4,12 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeT
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.provider.CsvSource
 
 class ApplicationTest : NsTest() {
 
@@ -43,11 +44,15 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
-    fun `자동차를 한칸 이동시킨다`() {
+    fun `0~9사이 무작위 정수가 4이상인 자동차들을 한칸 이동시킨다`() {
         assertSimpleTest {
-            val car = Car.withStartPosition("jun")
-            val movedCar = Car("jun", 1)
-            assertThat(car.moveForward()).isEqualTo(movedCar)
+            val cars = createUniqueCars(listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"))
+            val expectedCars = listOf(
+                cars[0], cars[1], cars[2], cars[3],
+                cars[4].moveForward(), cars[5].moveForward(), cars[6].moveForward(),
+                cars[7].moveForward(), cars[8].moveForward(), cars[9].moveForward(),
+            )
+            assertThat(cars.tryMoveForward((0..9).toList())).isEqualTo(expectedCars)
         }
     }
 
