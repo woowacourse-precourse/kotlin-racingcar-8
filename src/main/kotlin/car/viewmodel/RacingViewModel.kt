81e831fd3e4b1b83ae44a8c783bcae00d5583456
parkhyomi <phyomi3@naver.com>
2025-10-26 23:06:50 +0900

    feat(ViewModel):오류 케이스 추가

--- src/main/kotlin/car/viewmodel/RacingViewModel.kt
+++ src/main/kotlin/car/viewmodel/RacingViewModel.kt
@@ -7,6 +7,7 @@ import car.model.RacingGame
 val ERROR_MESSAGE_CAR_NAME_LENGTH = "자동차 이름은 5자를 초과할 수 없습니다."
 val ERROR_MESSAGE_ROUND_COUNT = "시도 횟수는 음수 일 수 없습니다."
 val ERROR_MESSAGE_DUPLICATE_CAR_NAME = "자동차 이름이 중복될 수 없습니다."
+val ERROR_MESSAGE_ZERO_COUNT = "횟수는 1회 이상 입력해야 합니다."
 
 class RacingViewModel (private val moveStrategy: MoveStrategy){
     private val game = RacingGame(moveStrategy)
@@ -40,5 +41,6 @@ class RacingViewModel (private val moveStrategy: MoveStrategy){
 
     private fun validateRoundCount(roundCount: Int) {
         require(roundCount >= 0) { ERROR_MESSAGE_ROUND_COUNT }
+        require(roundCount>1){ ERROR_MESSAGE_ZERO_COUNT }
     }
 }
