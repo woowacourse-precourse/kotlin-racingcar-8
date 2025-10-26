f6305c5b5697b2d22436ce9b48e3828dc730b748
parkhyomi <phyomi3@naver.com>
2025-10-26 18:31:04 +0900

    feat(View):시도 횟수 입력

--- src/main/kotlin/car/view/InputView.kt
+++ src/main/kotlin/car/view/InputView.kt
@@ -7,4 +7,8 @@ class InputView {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         return Console.readLine().split(",").map{it.trim()}
     }
+    fun readRacingCount(): Int {
+        println("시도할 횟수는 몇회인가요?")
+        return Console.readLine().toInt()
+    }
 }
