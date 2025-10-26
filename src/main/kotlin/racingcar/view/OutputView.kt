a5bacf2723c4a2b4730e4c8f3315fb66e476609c
hanminjae <hanminjae131@gmail.com>
2025-10-27 00:34:54 +0900

    feat: View 구현 (입출력 및 게임 흐름 제어)

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,23 @@
+package racingcar.view
+
+import racingcar.domain.Car
+
+object OutputView {
+    fun printCarNamesPrompt() = println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+
+    fun printRaceCountPrompt() = println("시도할 횟수는 몇 회인가요?")
+
+    fun printStartMessage() = println("\n실행 결과")
+
+    fun printRoundCars(cars: List<Car>) {
+        cars.forEach { car ->
+            println("${car.name} : ${car.getPositionString()}")
+        }
+    }
+
+    fun printBlankLine() = println()
+
+    fun printWinners(winnerNames: List<String>) = println("최종 우승자 : ${winnerNames.joinToString(", ")}")
+
+    fun printError(message: String?) = println("[ERROR] $message")
+}
---
+++ src/main/kotlin/racingcar/view/RacingGameView.kt
@@ -0,0 +1,41 @@
+package racingcar.view
+
+import racingcar.domain.MoveStrategy
+import racingcar.viewmodel.RacingGameViewModel
+
+class RacingGameView(
+    private val viewModel: RacingGameViewModel,
+    private val moveStrategy: MoveStrategy
+) {
+    fun start() {
+        try {
+            setupGame()
+            playGame()
+            showResult()
+        } catch (e: IllegalArgumentException) {
+            OutputView.printError(e.message)
+        }
+    }
+
+    private fun setupGame() {
+        OutputView.printCarNamesPrompt()
+        val carNames = InputView.readLine()
+        OutputView.printRaceCountPrompt()
+        val raceCount = InputView.readLine()
+        viewModel.setupGame(carNames, raceCount)
+    }
+
+    private fun playGame() {
+        OutputView.printStartMessage()
+        repeat(viewModel.getRaceCount()) {
+            val cars = viewModel.playOneRound(moveStrategy)
+            OutputView.printRoundCars(cars)
+            OutputView.printBlankLine()
+        }
+    }
+
+    private fun showResult() {
+        val winnerNames = viewModel.getWinnerNames()
+        OutputView.printWinners(winnerNames)
+    }
+}
