55ad183d1cf1e249f1aae16648794d8a7c7b30a1
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-26 18:30:09 +0900

    fix(InputValidator.kt): 잘못된 로직 고침
    - 자동차의 이름이 빈값이 아니라면 IllegalArgumentException 발생하는 코드 고침
    - 자동차의 이름이 5자 이하로 주어지면 IllegalArgumentException 발생 코드 고침
    - 이동횟수가 빈칸으로 주어지지 않으면 IllegalArgumentException 발생 코드 고침

--- src/main/kotlin/racingcar/InputValidator.kt
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -4,14 +4,14 @@ class InputValidator {
     fun validateCarName(carNames: List<String>) {
         require(carNames.isNotEmpty()) { "자동차의 이름을 입력하셔야 합니다" }
         for (carName in carNames) {
-            require(carName.isEmpty()) { "자동차의 이름을 입력하셔야 합니다" }
             require(carName.isNotBlank()) { "자동차의 이름은 빈칸일 수 없습니다" }
-            require(carName.length > MAX_CAR_NAME_LENGTH) { "자동차의 이름은 5글자 이하여야 합니다" }
+            require(carName.length <= MAX_CAR_NAME_LENGTH) { "자동차의 이름은 5글자 이하여야 합니다" }
         }
     }
 
     fun validateTotalMovement(totalMovement: String) {
-        require(totalMovement.isEmpty()) { "이동 횟수를 입력하셔야 합니다" }
+        require(totalMovement.isNotEmpty()) { "이동 횟수를 입력하셔야 합니다" }
+        require(totalMovement.isNotBlank()) { "빈칸으로 주어지면 안됩니다" }
         try {
             totalMovement.toInt()
         } catch (e: NumberFormatException) {
