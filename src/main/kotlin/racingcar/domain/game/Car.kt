e55681a326a2a0ff12a117dd3448007eee4efcbe
xodyd123 <gksrmf9186@naver.com>
2025-10-26 23:36:56 +0900

    refactor(car): position을 읽기 전용(private setter)으로 변경

--- src/main/kotlin/racingcar/domain/game/Car.kt
+++ src/main/kotlin/racingcar/domain/game/Car.kt
@@ -1,26 +1,22 @@
 package racingcar.domain.game
 
 class Car(private val name: String) {
-    private var position = 0
+    private var _position = 0
+    val position: Int get() = _position
 
     fun race(numberProvider: NumberProvider, moveRule: MoveRule) {
         val baseLine = numberProvider.generateNumbers()
         if (moveRule.canMove(baseLine))
             move()
-        stop()
     }
 
     private fun move() {
-        position++
-        return
-    }
-
-    private fun stop() {
+        _position++
         return
     }
 
     override fun toString(): String {
-        return name + " : " + "-".repeat(position);
+        return name + " : " + "-".repeat(position)
     }
 
 }
