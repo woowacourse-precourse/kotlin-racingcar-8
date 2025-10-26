4ec4b9c9770a770d69738b5b7e834ec1b283695c
WooJin Kong <kongwoojin03@gmail.com>
2025-10-26 12:33:40 +0900

    add: add readNames()

--- src/main/kotlin/racingcar/InputUtil.kt
+++ src/main/kotlin/racingcar/InputUtil.kt
@@ -1,3 +1,10 @@
 package racingcar
 
-object InputUtil
+import camp.nextstep.edu.missionutils.Console
+
+object InputUtil {
+    fun readNames(): List<String> {
+        val list = Console.readLine().split(",")
+        return list
+    }
+}
