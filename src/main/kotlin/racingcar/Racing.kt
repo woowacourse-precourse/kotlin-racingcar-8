bdb94980966fc191a91ace4e581b2dc1b11222c4
WooJin Kong <kongwoojin03@gmail.com>
2025-10-26 12:40:30 +0900

    add: Add Racing class

---
+++ src/main/kotlin/racingcar/Racing.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+class Racing(val list: List<Car>, val count: Int) {
+    fun play() {}
+
+    private fun go() {}
+
+    private fun printState() {}
+
+    private fun printWinner() {}
+}
