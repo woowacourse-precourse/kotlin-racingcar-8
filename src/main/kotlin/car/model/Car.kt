ac1397bd7c1a0c74c5edcbffdaf605c238ac1de1
parkhyomi <phyomi3@naver.com>
2025-10-26 19:23:10 +0900

    feat(Model):전진 카운트 및 - 대체 함수

--- src/main/kotlin/car/model/Car.kt
+++ src/main/kotlin/car/model/Car.kt
@@ -3,4 +3,12 @@ package car.model
 data class Car(
     val name: String,
     var position: Int = 0
-)
+){
+    fun move(){
+        position+=1
+    }
+
+    fun getPositionDisplay(): String {
+        return "-".repeat(position)
+    }
+}
