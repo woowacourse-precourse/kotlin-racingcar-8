c2d609fc265365a1228dd80d21e302e46f5d7955
CommitTheKermit <qnf323@naver.com>
2025-10-27 01:34:57 +0900

    refactor: 사용하지 않는 상수 정리

--- src/main/kotlin/racingcar/Messages.kt
+++ src/main/kotlin/racingcar/Messages.kt
@@ -6,14 +6,11 @@ object Messages {
 
     const val RACING_RESULT = "\n실행 결과"
     const val WINNER_PREFIX  = "최종 우승자 : "
-    const val ERROR_PREFIX = "[ERROR] : "
-    const val ERROR_DEFAULT = "잘못된 입력입니다."
 
     const val NOT_EMPTY_NAME = "이름은 비어 있을 수 없습니다."
     const val NOT_BIGGER_THAN_FIVE = "이름은 5자 이하여야 합니다."
 
     const val NOT_INTEGER = "정수가 아닌 입력값입니다."
     const val NOT_SMALLER_THAN_ZERO = "횟수는 0보다 큰 정수이여야 합니다."
-    const val DUPLICATE_NAME = "중복된 이름이 있습니다."
 
 }
