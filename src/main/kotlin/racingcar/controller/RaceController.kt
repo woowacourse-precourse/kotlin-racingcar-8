f6925ee5d6a29da6e2266a9a5babf34e4c3d0b5e
hoon4041 <simjaehoon3@naver.com>
2025-10-26 18:02:34 +0900

    refactor: RaceController의 경주 진행 로직 분리

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -10,23 +10,38 @@ object RaceController {
     private val inputParser = InputParser()
 
     fun startRace() {
+        val (carList, time) = initializeGame()
+
+        carManager.addCar(carList)
+        runRace(carList, time)
+
+        val winner = carManager.returnResult()
+        OutputView.finalResult(winner)
+    }
+
+    private fun initializeGame(): Pair<List<String>, Int> {
         val input = InputView.chooseCar()
         val carList = inputParser.parseCar(input)
 
         val tryTime = InputView.tryTime()
         val time = inputParser.parseTime(tryTime)
 
-        carManager.addCar(carList)
+        return Pair(carList, time)
+    }
+
+    private fun runRace(carList: List<String>, time: Int) {
         OutputView.printResult()
         repeat(time) {
-            carList.forEach { car ->
-                carManager.moveCar(car)
-                val location = carManager.getCarLocation(car)
-                OutputView.processResult(car, location)
-            }
+            raceSingleTime(carList)
             OutputView.printEnter()
         }
-        val winner = carManager.returnResult()
-        OutputView.finalResult(winner)
+    }
+
+    private fun raceSingleTime(carList: List<String>) {
+        carList.forEach { car ->
+            carManager.moveCar(car)
+            val location = carManager.getCarLocation(car)
+            OutputView.processResult(car, location)
+        }
     }
 }
