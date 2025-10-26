2d2bc18be2791a49d4901b2722767a4e9bf8a078
jangdaehan <wkdeogks17@gmail.com>
2025-10-27 04:06:58 +0900

    test(round): add 5-round game scenario with pobi and woni winning

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ValueSource
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 
 class ApplicationTest : NsTest() {
 
@@ -122,6 +124,14 @@ class ApplicationTest : NsTest() {
         }
     }
 
+    @ParameterizedTest
+    @MethodSource("slowMotionRacing")
+    fun `pobi와 jun이 승리하는 5턴 전체 게임 플레이`(currentRound: Round, randomNumbers: List<Int>, winner: List<Car>) {
+        assertSimpleTest {
+            assertThat(currentRound.tryMoveForward(randomNumbers).getWinners()).isEqualTo(winner)
+        }
+    }
+
 //    @Test
 //    fun `기능 테스트`() {
 //        assertRandomNumberInRangeTest(
@@ -143,9 +153,40 @@ class ApplicationTest : NsTest() {
     override fun runMain() {
         main()
     }
-//
-//    companion object {
-//        private const val MOVING_FORWARD: Int = 4
-//        private const val STOP: Int = 3
-//    }
+
+    companion object {
+        private const val MOVING_FORWARD: Int = 4
+        private const val STOP: Int = 3
+
+        @JvmStatic
+        private fun slowMotionRacing(): List<Arguments> {
+            return listOf(
+                Arguments.of(
+                    Round(0, listOf(Car("pobi", 0), Car("jun", 0), Car("woni", 0))),
+                    listOf(4, 3, 4),
+                    listOf(Car("pobi", 1), Car("woni", 1))
+                ),
+                Arguments.of(
+                    Round(0, listOf(Car("pobi", 1), Car("jun", 0), Car("woni", 1))),
+                    listOf(4, 4, 4),
+                    listOf(Car("pobi", 2), Car("woni", 2))
+                ),
+                Arguments.of(
+                    Round(0, listOf(Car("pobi", 2), Car("jun", 1), Car("woni", 2))),
+                    listOf(4, 4, 4),
+                    listOf(Car("pobi", 3), Car("woni", 3))
+                ),
+                Arguments.of(
+                    Round(0, listOf(Car("pobi", 3), Car("jun", 2), Car("woni", 3))),
+                    listOf(4, 3, 4),
+                    listOf(Car("pobi", 4), Car("woni", 4))
+                ),
+                Arguments.of(
+                    Round(0, listOf(Car("pobi", 4), Car("jun", 3), Car("woni", 4))),
+                    listOf(4, 4, 4),
+                    listOf(Car("pobi", 5), Car("woni", 5))
+                ),
+            )
+        }
+    }
 }
