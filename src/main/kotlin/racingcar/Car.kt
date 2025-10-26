b279cde97bf3c666f352472b8568db52fcfb0ae6
이주형 <leewngud980411@gmail.com>
2025-10-27 00:02:49 +0900

    feat(Car): 차 움직임 - 각 자동차는 0부터 9까지의 랜덤 숫자를 생성한다. - 생성된 숫자가 4 이상일 경우 자동차는 한 칸 전진한다. - 생성된 숫자가 4 미만일 경우 자동차는 움직이지 않는다.

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,11 +1,22 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car {
     val carsPosition: MutableMap<String, Int> = mutableMapOf()
 
     fun addCar(carNames: List<String>) {
         for (name in carNames) {
             carsPosition[name] = 0
+        }
+    }
+
+    fun moveCar() {
+        for (name in carsPosition.keys) {
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            if (randomNumber >= 4) {
+                carsPosition[name] = carsPosition[name]!! + 1
+            }
         }
     }
 }
