81d7532c66d748e976a0900bd1daa0f589ef3ec9
WooJin Kong <kongwoojin03@gmail.com>
2025-10-26 12:48:43 +0900

    fix: Throw IllegalArgumentException() if player name length larger than 5

--- src/main/kotlin/racingcar/InputUtil.kt
+++ src/main/kotlin/racingcar/InputUtil.kt
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Console
 object InputUtil {
     fun readNames(): List<String> {
         val list = Console.readLine().split(",")
+        list.forEach {
+            if (it.length > 5) throw IllegalArgumentException()
+        }
         return list
     }
 
