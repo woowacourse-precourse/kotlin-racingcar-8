2459961054e3ef27c38b3e7d3cbac783a97a563d
110lily <anmy121212@gmail.com>
2025-10-26 16:57:57 +0900

    feat: Car 클래스에 랜덤 값 기반 이동 로직 구현
    0부터 9 사이의 무작위 값을 생성하여 4 이상일 경우 position을 1 증가시키는 move() 함수를 Car 클래스에 추가.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,21 @@
 package racingcar
 
-data class Car(val name: String, var position: Int = 0)
+import camp.nextstep.edu.missionutils.Randoms
+
+data class Car(val name: String, var position: Int = 0) {
+
+    fun move() {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+
+        if (isMovable(randomNumber)) {
+            position++
+        }
+    }
+
+    private fun isMovable(randomNumber: Int): Boolean {
+        return randomNumber >= 4
+    }
+}
 
 fun main() {
     // TODO: 프로그램 구현
