22ae2080d9b6b2059248e97f1722a5c401e6cca1
Soldbone <1472ezs@gmail.com>
2025-10-26 18:37:11 +0900

    refactor: 함수 기능 분리 및 최소화
    기능 분리
    - RacingGame.play() 하위 로직을 함수로 세분화
    - RacingGame.play()에서는 경주만 진행하도록 수정
    - RacingGame.getWinners() 추가로 RacingGame은 데이터만 제공
    - 최종 우승자 출력은 printOutput(List<Car>)에서 담당하도록 분리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,19 +5,16 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     // TODO: 프로그램 구현
-
     val carNamesInput = getInputCarNames()
     val splitCarNames = splitCarName(carNamesInput)
-
     val round = getInputRound()
 
     val carInstances = splitCarNames.map { Car(it) }
     val racingGame = RacingGame(round, carInstances)
+    racingGame.play()
+    val winners = racingGame.getWinners()
 
-    val winners = racingGame.play()
-    val winnerNames = winners.joinToString(separator = ", ") { it.name }
-    println("최종 우승자 : $winnerNames")
-
+    printOutput(winners)
 }
 
 private fun getInputCarNames(): String {
@@ -47,37 +44,36 @@ private fun getInputRound(): Int {
     return round
 }
 
-private fun stringToInt(roundInput: String): Int =
-    if (roundInput.isNotBlank()) {
-        roundInput.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
-    } else {
-        0
-    }
+private fun stringToInt(roundInput: String): Int = if (roundInput.isNotBlank()) {
+    roundInput.toIntOrNull() ?: throw IllegalArgumentException("숫자를 입력해주세요.")
+} else {
+    0
+}
+
+private fun printOutput(winners: List<Car>?) {
+    val winnerNames = winners?.joinToString(separator = ", ") { it.name }
+    println("최종 우승자 : $winnerNames")
+}
 
 data class Car(val name: String) {
     var position: Int = 0
 }
 
 class RacingGame(val round: Int, val cars: List<Car>) {
-    var currentRound: Int = 1
+    private var currentRound: Int = 1
 
-    fun play(): List<Car> {
+    fun play() {
         println()
         println("실행 결과")
         while (!this.isFinished()) {
-            cars.map {
-                val numToMove = Randoms.pickNumberInRange(0, 9)
-                if (numToMove >= 4) {
-                    it.position++
-                }
-            }
-            cars.map {
-                println("${it.name} : ${"-".repeat(it.position)} ")
-            }
-            println()
-            this.currentRound++
+            this.makeMove()
+            this.printRoundResult(cars)
+            this.increaseCurrentRound()
         }
+    }
 
+    fun getWinners(): List<Car> {
+        check(this.isFinished()) { "게임이 아직 시작되지 않았습니다." }
         val winner = cars.maxBy { it.position }
         val winners = cars.filter { it.position == winner.position }
 
@@ -85,4 +81,34 @@ class RacingGame(val round: Int, val cars: List<Car>) {
     }
 
     private fun isFinished(): Boolean = this.currentRound > this.round
+
+    private fun makeMove() {
+        cars.map {
+            val randomNumber = this.getRandomNumber()
+            if (canMove(randomNumber)) {
+                increaseCarPosition(it)
+            }
+        }
+    }
+
+    private fun getRandomNumber(): Int {
+        return Randoms.pickNumberInRange(0, 9)
+    }
+
+    private fun canMove(randomNumber: Int): Boolean = randomNumber >= 4
+
+    private fun increaseCarPosition(car: Car) {
+        car.position++
+    }
+
+    private fun increaseCurrentRound() {
+        this.currentRound++
+    }
+
+    private fun printRoundResult(cars: List<Car>) {
+        cars.map {
+            println("${it.name} : ${"-".repeat(it.position)} ")
+        }
+        println()
+    }
 }
