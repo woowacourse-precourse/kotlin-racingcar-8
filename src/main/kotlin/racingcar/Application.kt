7b1a486b1708127f1a3da9272848bdb06bf52419
hanminjae <hanminjae131@gmail.com>
2025-10-27 00:36:42 +0900

    feat: Application 구현 (의존성 주입 및 실행)

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,15 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+import racingcar.domain.MoveStrategy
+import racingcar.domain.RacingGameValidator
+import racingcar.view.RacingGameView
+import racingcar.viewmodel.RacingGameViewModel
+
 fun main() {
-    // TODO: 프로그램 구현
+    val gameView = RacingGameView(
+        viewModel = RacingGameViewModel(RacingGameValidator),
+        moveStrategy = MoveStrategy { Randoms.pickNumberInRange(0, 9) >= 4 }
+    )
+    gameView.start()
 }
