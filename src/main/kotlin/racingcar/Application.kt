a1ed6fb16f67d1622cb9d1dc4de80dff180e1fdc
parkhyomi <phyomi3@naver.com>
2025-10-26 18:27:44 +0900

    feat(Application):입력 함수 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,18 @@
 package racingcar
 
-fun main() {
+import car.view.InputView
+import car.viewmodel.RacingViewModel
 
+fun main() {
+    Application().run()
+}
+
+class Application {
+    fun run() {
+        val inputView = InputView()
+        val viewModel = RacingViewModel()
+
+        val carNames = inputView.readCarNames()
+        viewModel.setupGame(carNames)
+    }
 }
