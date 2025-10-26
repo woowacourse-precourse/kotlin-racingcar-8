66c3c52123afdc48e381d18425c840684f9efddc
jangdaehan <wkdeogks17@gmail.com>
2025-10-27 04:33:56 +0900

    test: restore the original provided test

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -1,5 +1,6 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
@@ -132,24 +133,24 @@ class ApplicationTest : NsTest() {
         }
     }
 
-//    @Test
-//    fun `기능 테스트`() {
-//        assertRandomNumberInRangeTest(
-//            {
-//                run("pobi,woni", "1")
-//                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
-//            },
-//            MOVING_FORWARD, STOP
-//        )
-//    }
-//
-//    @Test
-//    fun `예외 테스트`() {
-//        assertSimpleTest {
-//            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
-//        }
-//    }
-//
+    @Test
+    fun `기능 테스트`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni", "1")
+                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi")
+            },
+            MOVING_FORWARD, STOP
+        )
+    }
+
+    @Test
+    fun `예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+        }
+    }
+
     override fun runMain() {
         main()
     }
