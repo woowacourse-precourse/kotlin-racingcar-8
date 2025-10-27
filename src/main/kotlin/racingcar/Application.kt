63ac05ce1545823a39391e9dae4f499a6fb209cf
김용승 <yongyong213@naver.com>
2025-10-27 20:34:41 +0900

    feat: 자동차 이름 받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,23 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+}
+
+fun getCarName(): List<String>{
+    println("경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분): ")
+    val input = Console.readLine() ?: throw IllegalArgumentException("입력이 없습니다.")
+    val names = input.split(",")
+
+    val it = names.iterator()
+
+    while(it.hasNext()){
+        val str = it.next().trim()
+        if (str.isBlank() || str.length > 5 ){
+            throw IllegalArgumentException("자동차 이름은 1자 이상, 5자 이하 입니다.")
+        }
+    }
+    return names
 }
