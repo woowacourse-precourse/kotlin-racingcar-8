f08e7241cf95a7902a70863e58e2d87735c3e8c9
박고운 <gounpark7475@gmail.com>
2025-10-27 17:52:22 +0900

    Application main 흐름 구성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,30 @@
 package racingcar
 
-fun main() {
-    // TODO: 프로그램 구현
+import camp.nextstep.edu.missionutils.Console
+
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+        val carNamesInput = Console.readLine()
+        val cars = CarFactory.createCars(carNamesInput)
+
+        println("시도할 횟수는 몇 회인가요?")
+        val attemptCountInput = Console.readLine()
+        val attemptCount = AttemptCountParser.parse(attemptCountInput)
+
+        println()
+        println("실행 결과")
+
+        val game = RacingGame(cars)
+
+        repeat(attemptCount) {
+            game.runOneRound()
+            OutputView.printRoundStatus(game.getCarsSnapshot())
+            println()
+        }
+
+        val winners = WinnerResolver.resolveWinners(game.getCarsSnapshot())
+        OutputView.printWinners(winners)
+    }
 }
