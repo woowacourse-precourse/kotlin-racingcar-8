384c89c5ea543179995e47bf2b2f5e00e6b7ed80
박고운 <gounpark7475@gmail.com>
2025-10-27 17:56:51 +0900

    depth 확인 및 함수 분리 주석 보완

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 object Application {
     @JvmStatic
     fun main(args: Array<String>) {
+        // 세부 로직(입력 파싱, 라운드 실행, 우승자 계산)은 각각의 객체가 함
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         val carNamesInput = Console.readLine()
         val cars = CarFactory.createCars(carNamesInput)
