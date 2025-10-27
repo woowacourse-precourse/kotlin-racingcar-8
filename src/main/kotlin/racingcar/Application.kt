d62347d3c742f00cef251800a7394a553efddeb8
iyungui <rsdbddml@gmail.com>
2025-10-27 14:11:22 +0900

    feat: 메인 애플리케이션 실행 로직 구현

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,25 @@
 package racingcar
 
+import racingcar.domain.Cars
+import racingcar.domain.RacingGame
+import racingcar.validator.InputValidator
+import racingcar.view.InputView
+
 fun main() {
-    // TODO: 프로그램 구현
+    val carNames = readCarNames()
+    val rounds = readRounds()
+
+    val cars = Cars.from(carNames)
+    val game = RacingGame(cars, rounds)
+    game.start()
+}
+
+private fun readCarNames(): List<String> {
+    val input = InputView.readCarNames()
+    return InputValidator.validateCarNames(input)
+}
+
+private fun readRounds(): Int {
+    val input = InputView.readRounds()
+    return InputValidator.validateRounds(input)
 }
