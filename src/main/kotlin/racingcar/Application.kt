59a9c1ae7b62d176ab127663f0a84c8e2e252bc1
SHC <hcshin0717@naver.com>
2025-10-27 19:00:18 +0900

    feat: enter car name and number of movements and distinguish exception situations

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,33 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
 fun main() {
-    // TODO: 프로그램 구현
+    val carInput = readLine()
+    val carList = carInput.split(",").map { name ->
+        invalidCarValueCheck(name)
+        Car(name)
+    }
+    val moveInput = readLine()
+    invalidMoveValueCheck(moveInput)
+    val moveCount = moveInput.toInt()
+}
+
 class Car(val name: String, var position: Int = 0) {
     fun move() {
         position++
     }
 }
+
+fun invalidCarValueCheck(input: String){
+    if (input.isEmpty() && input.length > 5) {
+        throw IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.")
+    }
+}
+
+fun invalidMoveValueCheck(input: String){
+    val count = input.toIntOrNull() ?: throw IllegalArgumentException("이동 횟수는 숫자여야 합니다.")
+    if (count < 0) {
+        throw IllegalArgumentException("이동 횟수는 0 이상이어야 합니다.")
+    }
 }
