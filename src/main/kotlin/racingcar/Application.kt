b9a8199a2ec45d10a9daf918d1f8d0997fdcd764
Jonghyeon Chang <hokhmah91@naver.com>
2025-10-27 22:23:16 +0900

    feat(validation): validate car names
    입력된 자동차 이름에 대한 유효성 검토 기능을 구현합니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,5 +6,19 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carNamesInput = Console.readLine()
 
-    // println("carNames: $carNamesInput")
+    validateCarNames(carNamesInput)
+}
+
+private fun validateCarNames(carNamesInput: String?) {
+    val names = carNamesInput?.split(",")
+        ?: throw IllegalArgumentException("입력이 없습니다.")
+
+    for (name in names) {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름이 비었습니다.")
+        }
+        if (name.length > 5) {
+            throw IllegalArgumentException("이름이 5자를 초과하였습니다.")
+        }
+    }
 }
