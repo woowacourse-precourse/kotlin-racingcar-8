b7983918f5c639ce8b439d411f2e923b14a1627a
imyhnam0 <yoonhyung9775@gmail.com>
2025-10-27 12:44:12 +0900

    feat: main 함수에 RacingController 실행 로직 추가

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -16,8 +16,9 @@ class CarTest {
     @Test
     fun `랜덤 값이 4 이상이면 전진한다`() {
         val car = Car("pobi")
+        val initialPosition = car.position
         repeat(100) { car.move() } // 여러 번 호출하여 적어도 일부 전진 확인
-        assertThat(car.position).isGreaterThanOrEqualTo(0)
+        assertThat(car.position).isGreaterThanOrEqualTo(initialPosition)
     }
 
     @Test
