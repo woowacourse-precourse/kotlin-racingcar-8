9790d583cdc7524ad7cb5217db1ad203a93dd8ed
박고운 <gounpark7475@gmail.com>
2025-10-27 17:44:35 +0900

    CarFactory에 trim 처리 추가

--- src/main/kotlin/racingcar/CarFactory.kt
+++ src/main/kotlin/racingcar/CarFactory.kt
@@ -3,7 +3,10 @@ package racingcar
 object CarFactory {
 
     fun createCars(rawNamesInput: String): List<Car> {
-        val names = rawNamesInput.split(",")
+        val names = rawNamesInput
+            .split(",")
+            .map { it.trim() }
+
         if (names.isEmpty()) {
             throw IllegalArgumentException("자동차 이름이 비어 있습니다.")
         }
