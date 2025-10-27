a150f8c2e7c05f354e33dd17f52380731b55cb2b
hoon4041 <simjaehoon3@naver.com>
2025-10-27 17:29:23 +0900

    refactor: ì 전체 구조 변ê의존성 주입을을 형식으ë로의 변경으로 인한 생성ì

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,7 +1,13 @@
 package racingcar
 
 import racingcar.controller.RaceController
+import racingcar.model.CarManager
+import racingcar.model.InputParser
+import racingcar.model.RandomNumberGenerator
+import racingcar.view.InputView
+import racingcar.view.OutputView
 
 fun main() {
-    RaceController.startRace()
+    val raceController = RaceController(CarManager(RandomNumberGenerator()), InputParser(), InputView, OutputView)
+    raceController.startRace()
 }
