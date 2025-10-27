685ccf68d891b8fa6c2033955cee73151a0afe9a
Jonghyeon Chang <hokhmah91@naver.com>
2025-10-27 23:21:05 +0900

    feat: implement race
    시도 횟수만큼 경주를 실행하고 각 시도별 경주 현황을 출력하는 기능을 구현합니다.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,12 +7,16 @@ fun main() {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     val carNamesInput = Console.readLine()
 
-    validateCarNames(carNamesInput)
+    val carNames = validateCarNames(carNamesInput)
 
     println("시도할 횟수는 몇 회인가요?")
     val tryCountInput = Console.readLine()
 
-    validateTryCount(tryCountInput)
+    val tryCount = validateTryCount(tryCountInput)
+
+    val cars = createCars(carNames)
+
+    runRace(cars, tryCount)
 }
 
 data class Car(
@@ -27,7 +31,7 @@ data class Car(
     }
 }
 
-private fun validateCarNames(carNamesInput: String?) {
+private fun validateCarNames(carNamesInput: String?): List<String> {
     val names = carNamesInput?.split(",")
         ?: throw IllegalArgumentException("입력이 없습니다.")
 
@@ -39,9 +43,11 @@ private fun validateCarNames(carNamesInput: String?) {
             throw IllegalArgumentException("이름이 5자를 초과하였습니다.")
         }
     }
+
+    return names
 }
 
-private fun validateTryCount(tryCountInput: String?) {
+private fun validateTryCount(tryCountInput: String?): Int {
     if (tryCountInput.isNullOrBlank()) {
         throw IllegalArgumentException("시도 횟수 입력을 안했습니다.")
     }
@@ -51,5 +57,26 @@ private fun validateTryCount(tryCountInput: String?) {
 
     if (count < 0) {
         throw IllegalArgumentException("입력된 시도 횟수가 음수입니다.")
+    }
+
+    return count
+}
+
+private fun createCars(carNames: List<String>): List<Car> {
+    return carNames.map { name -> Car(name.trim()) }
+}
+
+private fun runRace(cars: List<Car>, tryCount: Int) {
+    println("\n실행 결과")
+
+    repeat(tryCount) {
+        for (car in cars) {
+            car.moveForward()
+        }
+        for (car in cars) {
+            val positionMarker = "-".repeat(car.position)
+            println("${car.name} : $positionMarker")
+        }
+        println()
     }
 }
