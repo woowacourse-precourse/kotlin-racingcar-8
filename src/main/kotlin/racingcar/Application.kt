1601c7aa2665bc7b9896c077a761511c42b400c5
souk0712 <souk0712@gmail.com>
2025-10-27 22:58:11 +0900

    feat(application): add input function
    - add input round function
    - add input car names function

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,28 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+}
+
+fun input(): Race {
+    val cars = inputCars()
+    val round = inputRound()
+    return Race(round, cars)
+}
+
+private fun inputRound(): Int {
+    println(Message.ROUND_GUIDE_MESSAGE)
+    val roundInput = Console.readLine()
+    val round = roundInput.toIntOrNull() ?: throw IllegalArgumentException(Message.ROUND_ERROR_MESSAGE)
+    return round
+}
+
+private fun inputCars(): List<Car> {
+    println(Message.RACING_CAR_NAME_GUIDE_MESSAGE)
+    val carNames = Console.readLine().split(",")
+    val cars = carNames.map {
+        Car(it.trim())
+    }
+    return cars
 }
