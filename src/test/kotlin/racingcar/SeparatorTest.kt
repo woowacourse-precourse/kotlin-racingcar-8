1e93d971937fb500f554c10a3177da7377aa5ea9
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-27 16:46:17 +0900

    refactor: 필요없는 라이브러리 및 의존성 삭제

--- src/test/kotlin/racingcar/SeparatorTest.kt
+++ src/test/kotlin/racingcar/SeparatorTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import java.io.File.separator
 
 class SeparatorTest {
     @Test
--- src/test/kotlin/racingcar/domain/JudgeTest.kt
+++ src/test/kotlin/racingcar/domain/JudgeTest.kt
@@ -1,7 +1,6 @@
 package racingcar.domain
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class JudgeTest {
