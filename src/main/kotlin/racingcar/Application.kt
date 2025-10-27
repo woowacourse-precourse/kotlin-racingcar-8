73cdb15e1acfbaeddcf37398f9bb6e68ee6aea6d
cloud-23k <k_bright_st@pusan.ac.kr>
2025-10-27 19:36:09 +0900

    feat(Application) : 전체 실행 로직 작성

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import racingcar.view.Input
+import racingcar.view.Output
+import racingcar.viewModel.CarRacingGameViewModel
+
 fun main() {
-    // TODO: 프로그램 구현
+    val input = Input()
+    val output = Output()
+    val gameViewModel = CarRacingGameViewModel()
+    val carNames = gameViewModel.validateCarName(input.getCarsName())
+    val tryCount = gameViewModel.validateTryCount(Input().getTryCount())
+
+    gameViewModel.gameSetting(carNames)
+
+    gameViewModel.runAllGames(tryCount).forEach { gameResult ->
+        output.printGamesResult(gameResult)
+    }
+
+    output.finalWinners(gameViewModel.getWinnerNames())
 }
