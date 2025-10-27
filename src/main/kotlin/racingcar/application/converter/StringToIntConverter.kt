553900a61218aa5318015aead37e9452f7643c6f
xodyd123 <gksrmf9186@naver.com>
2025-10-27 09:37:34 +0900

    feat(converter): 시도 횟수가 0이하의 숫자일때 예외 처리 기능 추가

--- src/main/kotlin/racingcar/application/converter/StringToIntConverter.kt
+++ src/main/kotlin/racingcar/application/converter/StringToIntConverter.kt
@@ -1,11 +1,15 @@
 package racingcar.application.converter
 
+import racingcar.domain.error.ErrorCode.ATTEMPT_COUNT_MUST_BE_GREATER_THAN_ZERO
 import racingcar.domain.error.ErrorCode.INVALID_ATTEMPT_COUNT_NUMBER
 
 class StringToIntConverter : Converter<Int> {
     override fun convert(input: String): Int {
         try {
-            input.toInt()
+            val convertInt = input.toInt()
+            if (convertInt <= 0) {
+                throw IllegalArgumentException(ATTEMPT_COUNT_MUST_BE_GREATER_THAN_ZERO)
+            }
         } catch (e: NumberFormatException) {
             throw IllegalArgumentException(INVALID_ATTEMPT_COUNT_NUMBER)
         }
--- src/main/kotlin/racingcar/domain/error/ErrorCode.kt
+++ src/main/kotlin/racingcar/domain/error/ErrorCode.kt
@@ -4,4 +4,5 @@ object ErrorCode {
     const val NAMES_MUST_BE_COMMA_SEPARATED: String = "[ERROR] 이름은 쉼표(,)로 구분해 입력하세요."
     const val INVALID_CAR_NAME_LENGTH: String = "[ERROR] 자동차 이름은 1~5자여야 합니다."
     const val INVALID_ATTEMPT_COUNT_NUMBER: String = "[ERROR] 유효하지 않은 시도 횟수입니다. 숫자만 입력해 주세요."
+    const val ATTEMPT_COUNT_MUST_BE_GREATER_THAN_ZERO: String = "[ERROR] 시도 횟수는 0보다 큰 양수여야 합니다."
 }
