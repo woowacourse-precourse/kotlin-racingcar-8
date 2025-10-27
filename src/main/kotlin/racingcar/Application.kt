b9bf12f3cddc0a05e7482b73412d76a2edcf8984
moon890309 <abcdefg8989@naver.com>
2025-10-27 11:24:19 +0900

    feat: 랜덤값을 통한 이동 횟수 만큼 전진

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,31 @@
 package racingcar
 
+import java.util.Random
+
 fun main() {
     val names = readln().split(",")
     var cars = mutableMapOf<String, Int>()
-    for (name in names) {
-        if (name.length >= 5) throw IllegalArgumentException()
+    for(name in names){
+        if(name.length >= 5) throw IllegalArgumentException()
         cars[name] = 0
     }
+
+    val num = readln().toInt()
+
+    repeat(num) {
+        for(name in names) {
+            getRandmoNum(name, cars)
+        }
+    }
+}
+private fun getRandmoNum(name: String, cars: MutableMap<String, Int>){
+    val num = Random().nextInt(10)
+    print("$name : ")
+    if(num >= 4){
+        repeat(num - 3) {
+            print("-")
+            cars[name] = cars[name]!! + 1
+        }
+    }
+    println()
 }
