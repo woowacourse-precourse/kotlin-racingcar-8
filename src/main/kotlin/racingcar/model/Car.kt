6c51740bbc2495f985aa1612bcc8d1221a5ff7be
byunghyunkim0 <bb05qudgus@gamil.com>
2025-10-27 18:58:11 +0900

    fix: 자동차 이름 5자 이하 유효성 검사 추가

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -6,6 +6,10 @@ data class Car(
     val name: String,
     var position: Int = 0
 ) {
+    init {
+        require(name.length <= 5) { throw IllegalArgumentException() }
+    }
+
     fun move() {
         if (Randoms.pickNumberInRange(0, 9) >= 4) position++
     }
