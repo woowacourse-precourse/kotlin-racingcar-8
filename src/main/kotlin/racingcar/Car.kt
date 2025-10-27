18f1566014019e02fbdadb14fc20b5a4c87afca6
이주형 <leewngud980411@gmail.com>
2025-10-27 15:00:16 +0900

    refact(Car): 테스트 코드를 위한 리팩토링

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,7 +2,7 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class Car {
+class Car(private val randomProvider: () -> Int = { Randoms.pickNumberInRange(0, 9) }) {
     val carsPosition: MutableMap<String, Int> = mutableMapOf()
 
     fun addCar(carNames: List<String>) {
@@ -13,7 +13,7 @@ class Car {
 
     fun moveCar() {
         for (name in carsPosition.keys) {
-            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            val randomNumber = randomProvider()
             if (randomNumber >= 4) {
                 carsPosition[name] = carsPosition[name]!! + 1
             }
