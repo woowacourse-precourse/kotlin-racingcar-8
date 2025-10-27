69e13571fdd6f8c5ba2f51a40862770695e2f68c
cloud-23k <k_bright_st@pusan.ac.kr>
2025-10-27 18:17:28 +0900

    feat(CarRacingGame): Model CarRacingGame 작성

--- src/main/kotlin/racingcar/model/CarRacingGame.kt
+++ src/main/kotlin/racingcar/model/CarRacingGame.kt
@@ -9,17 +9,4 @@ class CarRacingGame(
         }
     }
 
-    fun getCurrentCarDistances(): Map<String, String>{
-        return carList.associate { car ->
-            car.name to car.getDistanceWithHyphen()
-        }
-    }
-
-    fun getWinners(): List<String> {
-        val maxPosition = carList.maxOfOrNull { it.getDistance() } ?: 0
-        return carList
-            .filter { it.getDistance() == maxPosition }
-            .map { it.name }
-    }
-
 }
