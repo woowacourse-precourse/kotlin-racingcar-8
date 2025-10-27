ca365b085d6880fc9dba641a593dee43ce2317e1
이주형 <leewngud980411@gmail.com>
2025-10-27 13:17:57 +0900

    feat(OutputHandler): 차 움직임 출력 - 각 자동차의 현재 위치를 화면에 출력한다. - 이동 결과는 시도 횟수만큼 반복 출력한다.

---
+++ src/main/kotlin/racingcar/OutputHandler.kt
@@ -0,0 +1,17 @@
+package racingcar
+
+class OutputHandler {
+
+    fun printRaceMessage() {
+        println()
+        println("실행 결과")
+    }
+
+    fun printRaceResult(carsPosition: Map<String, Int>) {
+        for ((name, position) in carsPosition) {
+            val progress = "-".repeat(position)
+            println("$name : $progress")
+        }
+        println()
+    }
+}
