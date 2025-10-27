cd24bf0f320c109aed561eea0322598a88316fd7
박고운 <gounpark7475@gmail.com>
2025-10-27 17:55:30 +0900

    test: 경계 상황(0회 시도, 공동 우승) 추가

--- src/main/kotlin/racingcar/WinnerResolverTest.kt
+++ src/main/kotlin/racingcar/WinnerResolverTest.kt
@@ -24,3 +24,19 @@ class WinnerResolverTest {
         assertEquals(listOf("pobi", "woni"), winners)
     }
 }
+
+class WinnerResolverEdgeCaseTest {
+
+    @Test
+    fun `아무도 이동하지 않은 경우 모든 자동차가 우승자다`() {
+        val a = Car("pobi")
+        val b = Car("woni")
+        val c = Car("jun")
+
+        // 모두 position 0
+        val winners = WinnerResolver.resolveWinners(listOf(a, b, c))
+
+        assertEquals(listOf("pobi", "woni", "jun"), winners)
+    }
+}
+
