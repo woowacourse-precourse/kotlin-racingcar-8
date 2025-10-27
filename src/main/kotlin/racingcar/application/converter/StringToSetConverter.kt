8d3dd82fbdb5c88d95714f45a791731b0f3f75ac
xodyd123 <gksrmf9186@naver.com>
2025-10-27 10:39:51 +0900

    feat(validation): 자동차 이름 중복 시 예외 처리
    - 입력을 Set으로 변환하면서 유일성 검사 수행
    
    - StringToSetConverter 도입
    
    - 중복 시 IllegalArgumentException 발생 (CAR_NAMES_MUST_BE_UNIQUE)

---
+++ src/main/kotlin/racingcar/application/converter/StringToSetConverter.kt
@@ -0,0 +1,18 @@
+package racingcar.application.converter
+
+import racingcar.domain.error.ErrorCode.CAR_NAMES_MUST_BE_UNIQUE
+import racingcar.domain.error.ErrorCode.INVALID_CAR_NAME_LENGTH
+
+private const val MAX_CAR_NAME_LENGTH = 5
+
+class StringToSetConverter : Converter<Set<String>> {
+    override fun convert(input: String): Set<String> {
+        val carNamesArr = input.split(",")
+        val carNamesSet = HashSet<String>()
+        for (name in carNamesArr) {
+            require(name.length <= MAX_CAR_NAME_LENGTH) { INVALID_CAR_NAME_LENGTH }
+            require(carNamesSet.add(name)) { CAR_NAMES_MUST_BE_UNIQUE }
+        }
+        return carNamesSet
+    }
+}
