9ee3af9a914cf76b011ddc7ace41ecc91d43dbcd
jangdaehan <wkdeogks17@gmail.com>
2025-10-27 21:24:24 +0900

    refactor(roundTest): remove unused import directive

--- src/test/kotlin/racingcar/RoundTest.kt
+++ src/test/kotlin/racingcar/RoundTest.kt
@@ -2,7 +2,6 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
