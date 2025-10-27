60fe0b4c8ecdb75c93542611d15948c54b3f93b2
cloud-23k <k_bright_st@pusan.ac.kr>
2025-10-27 18:44:36 +0900

    fix(CarRacingGameViewModel): 입력값 검증 코드 작성

---
+++ src/main/kotlin/racingcar/viewModel/CarRacingGameViewModel.kt
@@ -0,0 +1,21 @@
+package racingcar.viewModel
+
+import racingcar.model.Car
+import racingcar.model.CarRacingGame
+
+class CarRacingGameViewModel {
+
+    private lateinit var game: CarRacingGame
+
+    fun validateCarName(carNames: String): List<Car> {
+        val carName = carNames.split(",").map { it.trim() }
+        return carName.map { Car(it) }
+    }
+
+    fun validateTryCount(tryCountString: String): Int {
+        val tryCount = tryCountString.toIntOrNull() ?: throw IllegalArgumentException("시도횟수가 올바르지 않습니다.")
+        require(tryCount > 0) { "시도 횟수는 자연수로 제합됩니다." }
+
+        return tryCount
+    }
+}
