06c04afcba418be808185f663329e08c846dc2db
Jo Eungyeong <joeungyeong23@gmail.com>
2025-10-27 19:50:32 +0900

    refactor(car): 불변 리스트로 생성하므로 backing property 제거

--- src/main/kotlin/racingcar/domain/car/Cars.kt
+++ src/main/kotlin/racingcar/domain/car/Cars.kt
@@ -1,10 +1,8 @@
 package racingcar.domain.car
 
 class Cars(
-    private val _cars: List<Car>
+    val cars: List<Car>
 ) {
-    val cars: List<Car> get() = _cars.toList()
-
     fun getWinners(): List<Car> {
         val max = cars.maxOf { it.distance }
         return cars.filter { it.distance == max }
